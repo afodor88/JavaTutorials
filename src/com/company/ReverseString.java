0b53dd199c885539865e211561859c08dc23da78
alexandru.fodor <afodor88@gmail.com>
2017-11-28 23:01:36 +0200

    adding reverse a string.

---
+++ src/com/company/ReverseString.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class ReverseString {
+
+    private static String reverse(String input){
+        char[] in = input.toCharArray();
+        int begin=0;
+        int end=in.length-1;
+        char temp;
+        while(end>begin){
+            temp = in[begin];
+            in[begin]=in[end];
+            in[end] = temp;
+            end--;
+            begin++;
+        }
+        return new String(in);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(reverse("test"));
+    }
+
+}
