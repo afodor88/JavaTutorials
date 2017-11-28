f187aafe39b2b67c8c082072d63121c73db4b729
alexandru.fodor <afodor88@gmail.com>
2017-11-28 23:29:43 +0200

    some rnaming.

--- src/com/company/ReverseString.java
+++ src/com/company/ReverseString.java
@@ -3,23 +3,29 @@ package com.company;
 public class ReverseString {
 
     private static String reverse(String input){
-        char[] in = input.toCharArray();
-        int begin=0;
-        int end=in.length-1;
+        char[] charList = input.toCharArray();
+
+        int begin = 0;
+        int end = charList.length - 1;
         char temp;
-        while(end>begin){
-            temp = in[begin];
-            in[begin]=in[end];
-            in[end] = temp;
+
+        while (end > begin){
+
+            temp = charList[begin];
+            charList[begin] = charList[end];
+            charList[end] = temp;
             end--;
             begin++;
         }
-        return new String(in);
+
+        return new String(charList);
+
     }
 
     public static void main(String[] args) {
 
-        System.out.println(reverse("test"));
+        System.out.println(reverse("abc"));
     }
 
 }
+
